b928276547d7f7a6ebcf32d8f5d7f11fc87f71d9
Alex Bolyakov <alex.bolyakov.crs@me.com>
2024-05-03 14:45:05 -0400

    pangram an longest-word is done

--- pangram.js
+++ pangram.js
@@ -1,12 +1,16 @@
-// Pangram Sentence
-// A pangram is a sentence that contains all the letters of the English alphabet at least once, like “The quick brown fox jumps over the lazy dog.”
-
-// Write a function to check a sentence to see if it is a pangram or not.
-
-// For example:
-
-// isPangram("The quick brown fox jumps over the lazy dog!");
-// // -> True
-
-// isPangram("I like cats, but not mice");
-// // -> False
+const isPangram = (string) => {
+    if (typeof string !== 'string') {
+      return 'It must be a string.'
+    }
+    const result = new Set()
+    for (const ch of string.toLowerCase()) {
+      if (/[a-z]/.test(ch)) {
+          result.add(ch)
+      }
+    }
+    return result.size === 26
+  }
+  console.log(isPangram("The quick brown fox jumps over the lazy dog!"));
+  console.log(isPangram(12356));
+  console.log(isPangram("I like cats, but not mice"));
+  console.log(isPangram("Waltz, bad nymph, for quick jigs vex."))
